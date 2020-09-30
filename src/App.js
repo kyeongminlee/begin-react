03bcdb07ed08f8b5da5b50b9b0c606b7c118b011
kyeongmin <kyeongmin.jake.lee@gmail.com>
2020-09-30 11:58:02 +0900

    19. React.memo 를 사용한 컴포넌트 리렌더링 방지

--- src/App.js
+++ src/App.js
@@ -54,25 +54,25 @@ function App() {
       username,
       email,
     };
-    setUsers([...users, user]);
+    setUsers((users) => [...users, user]);
     setInputs({
       username: "",
       email: "",
     });
     nextId.current += 1;
-  }, [username, email, users]);
+  }, [username, email]);
 
-  const onRemove = (id) => {
-    setUsers(users.filter((user) => user.id !== id));
-  };
+  const onRemove = useCallback((id) => {
+    setUsers((users) => users.filter((user) => user.id !== id));
+  }, []);
 
-  const onToggle = (id) => {
-    setUsers(
+  const onToggle = useCallback((id) => {
+    setUsers((users) =>
       users.map((user) =>
         user.id === id ? { ...user, active: !user.active } : user
       )
     );
-  };
+  }, []);
 
   const count = useMemo(() => countActiveUsers(users), [users]);
 
